ae8aed3a698bde9f8662ac5bc7311f0fa48e7393
Sal <user@example.com>
2025-05-19 14:00:45 +0200

    Start of project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,13 @@ package org.example;
 
 
 import org.example.ihm.Ihm;
+import org.example.model.TodoServiceImpl;
 import org.example.service.TodoService;
 
 
 public class Main {
     public static void main(String[] args) {
-        TodoService service = null; // remplacer null par une instanciation d'une classe respectant l'interface TodoService
+        TodoService service = new TodoServiceImpl();
         Ihm ihm = new Ihm(service);
         ihm.start();
     }
--- src/main/java/org/example/model/Todo.java
+++ src/main/java/org/example/model/Todo.java
@@ -1,5 +1,40 @@
 package org.example.model;
 
 public class Todo {
+    private int id;
+    private String name;
+    private char description;
+    private boolean done;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getDescription() {
+        return description;
+    }
+
+    public void setDescription(char description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
